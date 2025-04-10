4977eb26dfb8d15091c52b3adb20b99dd17a1c54
Aakash <aakashp31102005@gmail.com>
2025-04-10 21:26:24 +0530

    git remote add origin https://github.com/aakashp31102005/dsa-practice.git

--- src/interview/arrayandString.java
+++ src/interview/arrayandString.java
@@ -1,39 +1,77 @@
 package interview;
 
 import javax.swing.plaf.IconUIResource;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class arrayandString {
 
     public static void main(String[] args) {
-        String str = "  hello  world moro";
-        String s = " ";
+        String str = "  hello world  ";
+        System.out.println(str.trim());
+        //---------------------------pivot index---------------------------------//
+        System.out.println(pivotIndex(new int[]{1,7,3,6,5,6}));
+        maxklensumsubarray();
+
+        //------------------------------------------sumregion------------------------//
+        NumMatrix nm=new NumMatrix(new int[][]{{3, 0, 1, 4, 2},{5, 6, 3, 2, 1}
+        ,{1, 2, 0, 1, 5},{4, 1, 0, 1, 7},{1, 0, 3, 0, 5}});
+        nm.sumRegion(2, 1, 4, 3);
+
+        //------------------------max-count in matrix-----------------------------------//
+        System.out.println(nm.maxCount(3,3,new int[][]{{2,2},{3,3}}));
+
+        //-------------------------reverse words in string without split and trim functions---------------------//
+        System.out.println(reverseWords(str));
+
+        //---------------------------find length of last words--------------------------------//
+        String var1="   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(var1));
+        }
+
+    public static String reverseWords(String s) {
+        String ss = " ";
         int i = 0;
-        String temp="";
-       List<String> al=new ArrayList<>();
-        while (i < str.length()) {
-            while (Character.toString(str.charAt(i)).hashCode() == s.hashCode()) {
+        String temp = "";
+        List<String> al=new ArrayList<>();
+        while (i < s.length()) {
+            while (i<s.length() && Character.toString(s.charAt(i)).hashCode() == ss.hashCode()) {
                 i++;
                 if (!temp.equals("")) {
                     al.addFirst(temp);
                     temp = "";
                 }
             }
-            temp+=str.charAt(i);
+            if(i< s.length()) {
+                temp += s.charAt(i);
+            }
             i++;
-            }
-            al.addFirst(temp);
-            String result="";
-            for(String element:al){
-                result+=element+" ";
-            }
-            System.out.println(result);
-        System.out.println(pivotIndex(new int[]{1,7,3,6,5,6}));
-
         }
+        if(!temp.isEmpty()) al.addFirst(temp);
+        String result="";
+        int index=0;
+        for(String element:al){
+            if(index<al.size()-1) {
+                result += element + " ";
+            }
+            index++;
+        }
+        result+=al.get(index-1);
+        return result;
+    }
+
+    public static int lengthOfLastWord(String s) {
+            String result=s.trim();
+            int count=0;
+            for(int i=result.length()-1;i>=0;i--){
+                if(!Character.toString(result.charAt(i)).equals(" ")){
+                    count++;
+                }else if(count != 0){
+                    break;
+                }
+            }
+            return count;
+    }
     public static int pivotIndex(int[] nums) {
 
             List<Integer> left=new ArrayList<>();
@@ -62,5 +100,54 @@ public class arrayandString {
         }
         return -1;
     }
-  
+    public static void maxklensumsubarray() {
+        int k = 3;
+        int[] arr = new int[]{100, 200, 300, 400};
+        int sum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            maxSum += arr[i];
+        }
+
+        int windowSum = maxSum;
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        System.out.println(maxSum);
+
+    }
+    }
+class NumMatrix {
+    int[][] matrix;
+    public NumMatrix(int[][] matrix) {
+        this.matrix=matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum=0;
+        int i=col1;
+        int j=row1;
+        while( j<=row2){
+            if(i<=col2){
+                sum+=matrix[j][i];
+                i++;
+            }else if(j<=row2 && i>col2){
+                j++;
+                i=col1;
+            }
+        }
+        return sum ;
+    }
+    public int maxCount(int m, int n, int[][] ops) {
+        int x=ops[0][1];
+        int y=ops[0][1];
+        for(int i=0;i<ops.length;i++){
+            x=Math.min(ops[i][0],x);
+            y=Math.min(ops[i][1],y);
+        }
+        return  x*y;
+    }
 }
+
