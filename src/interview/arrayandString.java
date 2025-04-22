e49b2ccb8adbd863729bcc32dce09682523185d1
Aakash <aakashp31102005@gmail.com>
2025-04-22 12:01:09 +0530

    git remote add origin https://github.com/aakashp31102005/dsa-practice.git

--- src/interview/arrayandString.java
+++ src/interview/arrayandString.java
@@ -1,5 +1,6 @@
 package interview;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import  java.math.BigInteger;
 import java.util.regex.Matcher;
@@ -88,7 +89,201 @@ public class arrayandString {
         //--------------------container with max water---------------------------//
             maxArea(new int[]{1,1});
     //---------------------squares of sorted array----------------------------//
-        sortedSquares(new int[]{-4,-1,0,3,10});
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-7,-3,2,3,11})));
+        //-------------------remove duplicates(fast and slow pointer)-----------------------//
+        removeDuplicates(new int[]{1,1,2});
+    //----------------------------------Max Sum Subarray of size K------------------------//
+    System.out.println(maximumSumSubarray(new int[]{100,200,300,400},2));
+
+    //----------------------------------min lenght of subbarray of sum equal to target-----------------------//
+        System.out.println(minSubArrayLen(7,new int[]{2,3,1,2,4,3}));
+    //---------------------------------------max avg sum within k----------------------------------//
+        System.out.println(findMaxAverage(new int[]{4,2,1,3,3},2));
+        double a= (double) 1 /2;
+        System.out.println(a);
+    //-------------------- checking k consecutive one and zeros---------------------------//
+        System.out.println(kconsecutive1s0s("000000000000000000100111101011000", 5));
+    //------------------------------------ Check If a String Contains All Binary Codes of Size K---/
+        System.out.println(hasAllCodes("0110",2));
+    //---------------------------longest ones---------------------------------//
+        System.out.println(longestOnes(new int[]{0,0,0,1},4));
+    //-----------------checkInclusion-----------------------------//
+        System.out.println(isPermutation("abc","bca"));
+
+        System.out.println(maxSatisfied(new int[]{1},new int[]{0},1));
+
+    }
+
+
+    public static  int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int base=0;
+            for(int i=0;i<grumpy.length;i++){
+                if(grumpy[i] != 1){
+                    base+=customers[i];
+                }
+            }
+            int recovery=Integer.MIN_VALUE;
+           for(int i=0;i<=grumpy.length-minutes;i++){
+               int wsum=0;
+               for(int j=i;j<i+minutes;j++){
+                   if(grumpy[j] == 1){
+                       wsum+=customers[j];
+                   }
+               }
+               recovery=Math.max(wsum,recovery);
+           }
+           return recovery+base;
+    }
+    public static boolean isPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] freq = new int[26]; // Only for lowercase letters
+
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+
+    public static int longestOnes(int[] nums, int k) {
+            int len=0;
+            int tempk=k;
+            int i=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j] == 0){
+                    tempk--;
+                }
+                if(tempk<0 || nums.length==k){
+                    int wlen;
+                    if(nums.length==k){
+                        wlen=j-i+1;
+                    }else wlen=j-i;
+                    len=Math.max(wlen,len);
+                }
+                while(tempk <0){
+                    if(nums[i] == 0){
+                        tempk++;
+                    }
+                    i++;
+                }
+            }
+            return len;
+    }
+    public static  boolean hasAllCodes(String s, int k) {
+        Set<String> set=new HashSet<>();
+        for(int i=0;i<=s.length()-k;i++){
+            String temp=s.substring(i,i+k);
+            if(!set.contains(temp)){
+                set.add(temp);
+            }
+        }
+        if (set.size()==Math.pow(2,k)){
+            return true;
+        }return false;
+    }
+
+    public static  boolean kconsecutive1s0s(String s, int k) {
+            int zp=1;
+            int op=1;
+            for(int i=1;i<s.length();i++){
+                if(s.charAt(i)==s.charAt(i-1)){
+                    if(zp!=k && s.charAt(i)=='0'){
+                        zp++;
+                    }else if(op !=k && s.charAt(i)=='1'){
+                        op++;
+                    }
+                }else{
+                    if(s.charAt(i)=='0' && zp!=k){
+                        zp=1;
+                    }else if(s.charAt(i)=='1' && op!=k){
+                        op=1;
+                    }
+                }
+            }
+            if(zp == k && op ==k){
+                return true;
+            }return false;
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+            int left=0;
+            double ravg;
+            int rsum=0;
+            for(int i=0;i<k;i++){
+                rsum+=nums[i];
+            }
+            ravg=(double) rsum/k;
+            for(int right=k;right<nums.length;right++){
+                int wsum=rsum+nums[right]-nums[left];
+                left++;
+                double wavg=(double) wsum/k;
+                ravg=Math.max(wavg,ravg);
+                rsum=wsum;
+            }
+            return ravg;
+    }
+        public static int minSubArrayLen(int target, int[] nums) {
+            int left = 0;
+            int track = target;
+            int minlen = Integer.MAX_VALUE;
+
+            for (int right = 0; right < nums.length; right++) {
+                track -= nums[right];
+                while (track <= 0) {
+                    int windowlen = right - left + 1;
+                    minlen = Math.min(minlen, windowlen);
+                    track += nums[left];
+                    left++;
+                }
+            }
+            return minlen == Integer.MAX_VALUE ? 0 : minlen;
+    }
+    public static int maximumSumSubarray(int[] arr, int k) {
+        //normal 2 loop bruteforce or navie solution o(n*k)
+//        int max=Integer.MIN_VALUE;
+//        for(int i=0;i<=arr.length-k;i++){
+//            int sum=0;
+//            for(int j=i;j<k+i;j++){
+//                sum+=arr[j];
+//            }
+//            max=Math.max(max,sum);
+//        }
+//        sliding window o(n)
+//step1:check the problems question satisfies sw condtion that is it require contigous block
+//step2:which pattern in sliding window :fixed size
+//step3:type of subpatter:sum,max,min,frequence,distinct:sum
+//step4:reference variable
+//step5:updatte reference variable value with running window value
+        int rv=0;
+        for(int i=0;i<k;i++){
+            rv+=arr[i];
+        }
+        int windowsum=rv;
+        for(int i=1;i<=arr.length-k;i++){
+            windowsum=windowsum-arr[i-1]+arr[k+i-1];
+            //update reference variable with computed window value
+            rv=Math.max(windowsum,rv);
+        }
+        return  rv;
+}
+
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+            nums[k++] = nums[i];
+        }
+
+        return k;
     }
     public static  int[] sortedSquares(int[] nums){
         for(int i=0;i<nums.length;i++){
@@ -97,15 +292,16 @@ public class arrayandString {
         int[] temp=new int[nums.length];
         int j=temp.length-1;
         int i=0;
-        while(i<temp.length && j>=0){
+        int k=temp.length-1;
+        while(i<temp.length && j>=0 && k>=0){
             if(nums[i]<nums[j]){
-                temp[j]=nums[j];
+                temp[k]=nums[j];
                 j--;
             }else{
-                temp[j]=nums[i];
+                temp[k]=nums[i];
                 i++;
-                j--;
             }
+            k--;
         }
         return temp;
     }
